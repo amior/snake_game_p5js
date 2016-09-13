fb5b7f8b847ebfe7ecd0ffe215639675b6fa8c61
Amio Rahman <amio.rhmn@gmail.com>
2016-09-13 22:05:59 +0600

    Add comments

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,6 @@
 var snake;
-var gridSize = 10; //Assign size of a grid
-var snakePieces = [];
+var gridSize = 10;    //Assign size of a grid
+var snakePieces = [];    //List to store all snake pieces
 
 function setup() {
 	createCanvas(600, 400);
@@ -16,16 +16,19 @@ function draw() {
 	food.display();
 	snake.move();
 
+	//When a food is eaten, extend the length of the snake and add new food at random position
 	if(snake.x==food.x && snake.y==food.y) {
 		snake.extend(food.x, food.y);
 		food = new Food(floor(random(0,width/gridSize))*gridSize, floor(random(0,height/gridSize))*gridSize);
 	}
 
+	//When the head of the snake hits the wall, freeze it
 	if(snake.x<0 || snake.x>width || snake.y<0 || snake.y>height) {
 		snake.moving = false;
 		Object.freeze(snake);
 	}
 
+	//When the head of the snake hits its other pieces, freeze it
 	for(var i=1; i<snakePieces.length; i++) {
 		if(snake.x==snakePieces[i].x && snake.y==snakePieces[i].y) {
 			snake.moving = false;
--- js/snake.js
+++ js/snake.js
@@ -1,9 +1,10 @@
 function Snake(x,y) {
 	this.x = x;
 	this.y = y;
-	this.direction; //holds value of the current direction
-	this.moving = true;
+	this.direction;    //Holds value of the current direction
+	this.moving = true;    //Whether snake is permitted to move or not
 
+	//Display the whole snake by displaying piece by piece
 	this.display = function () {
 		fill(255);
 		for(var i=0; i<snakePieces.length; i++) {
@@ -11,7 +12,9 @@ function Snake(x,y) {
 		}
 	}
 
+	//Move the snake head according to keypress
 	this.move = function () {
+		//Every piece other than head piece gets the positon of its previous piece
 		if(this.moving) {
 			for (var i = snakePieces.length-1; i > 0; i--) {
 				snakePieces[i].x = snakePieces[i-1].x;
@@ -20,57 +23,72 @@ function Snake(x,y) {
 		}
 
 		if(keyCode == LEFT_ARROW) {
+			//When previous direction is not right, turn the head to the left of screen
 			if(this.direction!="right") {
 				this.x = this.x - gridSize;
 				this.direction = "left";
 			}
+			//When previous direction is right, continue to move right
 			else {
 				this.x = this.x + gridSize;
 			}
 		}
 		else if(keyCode == RIGHT_ARROW) {
+			//When previous direction is not left, turn the head to the right of screen
 			if(this.direction!="left") {
 				this.x = this.x + gridSize;
 				this.direction = "right";
 			}
+			//When previous direction is left, continue to move left
 			else {
 				this.x = this.x -gridSize;
 			}
 		}
 		else if(keyCode == UP_ARROW) {
+			//When previous direction is not down, turn the head to the up-side of screen
 			if(this.direction!="down") {
 				this.y = this.y - gridSize;
 				this.direction = "up";
 			}
+			//When previous direction is down, continue to move down
 			else {
 				this.y = this.y + gridSize;
 			}
 		}
 		else if(keyCode == DOWN_ARROW) {
+			//When previous direction is not up, turn the head to the down-side of screen
 			if(this.direction!="up") {
 				this.y = this.y + gridSize;
 				this.direction = "down";
 			}
+			//When previous direction is up, continue to move up
 			else {
 				this.y = this.y - gridSize;
 			}
 		}
+		//When any other key is pressed
 		else {
+			//When previous direction is left, continue to move left
 			if(this.direction=="left") {
 				this.x = this.x - gridSize;
 			}
+			//When previous direction is right, continue to move right
 			else if(this.direction=="right") {
 				this.x = this.x + gridSize;
 			}
+			//When previous direction is up, continue to move up
 			else if(this.direction=="up") {
 				this.y = this.y - gridSize;
 			}
+			//When previous direction is down, continue to move down
 			else if(this.direction=="down") {
 				this.y = this.y + gridSize;
 			}
 		}
 	}
 
+	//Extend the length of the snake
+	//x and y is the coordinate of the food that has been eaten
 	this.extend = function(x, y) {
 		if(this.direction=="left") {
 			newPiece = new Piece(x+gridSize, y);
