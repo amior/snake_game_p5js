2f3b9e9b7833a67b675b725baea85c71b22d9f5c
Amio Rahman <amio.rhmn@gmail.com>
2016-09-13 23:27:15 +0600

    Show score and game-over massage

--- js/sketch.js
+++ js/sketch.js
@@ -1,10 +1,12 @@
-var snake;
 var gridSize = 10;    //Assign size of a grid
+var score = 0;
+var snake;
 var snakePieces = [];    //List to store all snake pieces
 
 function setup() {
 	createCanvas(600, 400);
-	frameRate(10);
+	frameRate(15);
+	
 	snake = new Snake(floor(random(0,width/gridSize))*gridSize, floor(random(0,height/gridSize))*gridSize);
 	snakePieces.push(snake);
 	food = new Food(floor(random(0,width/gridSize))*gridSize, floor(random(0,height/gridSize))*gridSize);
@@ -12,28 +14,43 @@ function setup() {
 
 function draw() {
 	background(0);
+
+	textSize(20);
+	fill(200, 120);
+	text("Score: " + score, width-90, 20);
+
 	snake.display();
 	food.display();
 	snake.move();
 
 	//When a food is eaten, extend the length of the snake and add new food at random position
 	if(snake.x==food.x && snake.y==food.y) {
+		score++;
 		snake.extend(food.x, food.y);
 		food = new Food(floor(random(0,width/gridSize))*gridSize, floor(random(0,height/gridSize))*gridSize);
 	}
 
-	//When the head of the snake hits the wall, freeze it
+	//Check if the head hits the wall
 	if(snake.x<0 || snake.x>width || snake.y<0 || snake.y>height) {
-		snake.moving = false;
-		Object.freeze(snake);
+		gameOver();
 	}
 
-	//When the head of the snake hits its other pieces, freeze it
+	//Check if the head hits the tail
 	for(var i=1; i<snakePieces.length; i++) {
 		if(snake.x==snakePieces[i].x && snake.y==snakePieces[i].y) {
-			snake.moving = false;
-			Object.freeze(snake);
+			gameOver();
 			break;
 		}
 	}
+}
+
+function gameOver() {
+	//freeze the snake
+	snake.moving = false;
+	Object.freeze(snake);
+
+	//Show Game Over massage
+	textSize(50);
+	fill(200, 120);
+	text("GAME OVER", width/2-150, height/2);
 }
